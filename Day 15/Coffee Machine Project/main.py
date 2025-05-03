3d418b5a1319de2e35fe3ae12bf7231a41f44c85
Prajna Shetty <124861452+prajnaa27@users.noreply.github.com>
2025-05-03 12:48:09 +0530

    Update main.py
    Added my version/approach of CoffeeMachine Project

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -1,3 +1,6 @@
+from importlib.resources import is_resource
+from statistics import quantiles
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -30,3 +33,81 @@ resources = {
     "coffee": 100,
 }
 
+def calculate_total_money(q,d,n,p):
+    total=0.25*q+0.1*d+0.05*n+0.01*p
+    return total
+
+def check_resources(coffe_type):
+    ingredients=MENU[coffe_type]['ingredients']
+    cost=MENU[coffe_type]['cost']
+    # print(ingredients)
+    # print(cost)
+    # print(resources)
+    for ingredient in ingredients:
+        # print(ingredient)
+        quantity=ingredients[ingredient]
+        if resources[ingredient]>=quantity:
+            # print("true")
+            continue
+        else:
+            print(f"Sorry there is not enough {ingredient}")
+            return False
+        # return True
+    return True
+        # print(quantity)
+
+def update_resources(coffee_t,total):
+    print(f"total {total}")
+    for key in resources:
+        if key=='money':
+            resources['money']=resources['money']+total
+        else:
+            if key in MENU[coffee_t]['ingredients']:
+                resources[key]=resources[key]-MENU[coffee_t]['ingredients'][key]
+            else:
+                continue
+
+def print_report():
+    measurements={'water':'ml','milk':'ml','coffee':'g','money':'$'}
+    for key in resources:
+        if key in measurements:
+            suffix=measurements[key]
+            # print(suffix)
+        print(f"{key.title()}:{resources[key]}{suffix}")
+    # print(resources)
+
+resources['money']=0
+def make_coffee():
+    action_completed = True
+    while action_completed:
+        coffee_type=input("What would you like? (espresso/latte/cappuccino):").lower()
+        global resources
+        if coffee_type=='off':
+            action_completed=True
+            exit(0)
+        if coffee_type=='report':
+            print_report()
+        if coffee_type=='espresso' or coffee_type=='latte' or coffee_type=='cappuccino':
+            is_resource=check_resources(coffee_type)
+            if is_resource:
+                print("Please insert coins.")
+                quarters=int(input("How many quarters?"))
+                dimes=int(input("How many dimes?"))
+                nickels=int(input("How many nickels?"))
+                pennies=int(input("How many pennies?"))
+                total=calculate_total_money(quarters,dimes,nickels,pennies)
+                # print(total)
+                cost=MENU[coffee_type]['cost']
+                # print(f"cost:{cost}")
+                if cost>total:
+                    print("Not enough money")
+                else:
+                    update_resources(coffee_type,cost)
+                    print(resources)
+                    change=round(total-cost,2)
+                    print(f"Here's ${change} in change")
+                    print(f"Here's your {coffee_type}, Please enjoy!")
+
+
+
+make_coffee()
